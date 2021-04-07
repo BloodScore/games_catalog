41d1c10e0cabe6d4048734a4b33c8383fcbc87eb
BloodScore <dmitrygolysh@gmail.com>
2021-04-07 11:43:58 +0300

    views and api wrapper fixed. combined filters & search work properly

--- pages/views.py
+++ pages/views.py
@@ -11,8 +11,6 @@ def index(request):
     genres_id = []
     platforms_id = []
 
-    print(genre, platform, rating)
-
     platforms_list = [platform for platform in api.get_platforms()]
     genres_list = [genre for genre in api.get_genres()]
 
@@ -30,49 +28,42 @@ def index(request):
                     platforms_id.append(i.get('id'))
                     break
 
-    if genre:
-        if platform:
-            if query:
-                games_list = api.get_games_test(f'search "{query}"; fields name, cover.url, genres.name; where genres = {str(genres_id)} & platforms = {str(platforms_id)} & rating >= {rating};')
-            else:
-                games_list = api.get_games_test(f'fields name, cover.url, genres.name; where genres = {str(genres_id)} & platforms = {str(platforms_id)} & rating >= {rating};')
-        else:
-            if query:
-                games_list = api.get_games_test(
-                    f'search "{query}"; fields name, cover.url, genres.name; where genres = {str(genres_id)} & rating >= {rating};')
-            else:
-                games_list = api.get_games_test(
-                    f'fields name, cover.url, genres.name; where genres = {str(genres_id)} & rating >= {rating};')
-    elif platform:
-        if query:
-            games_list = api.get_games_test(f'search "{query}"; fields name, cover.url, genres.name; where platforms = {str(platforms_id)} & rating >= {rating};')
-        else:
-            games_list = api.get_games_test(
-                f'fields name, cover.url, genres.name; where platforms = {str(platforms_id)} & rating >= {rating};')
-    elif rating:
-        if query:
-            games_list = api.get_games_test(f'search "{query}"; fields name, cover.url, genres.name; where rating >= {rating};')
-        else:
-            games_list = api.get_games_test(
-                f'fields name, cover.url, genres.name; where rating >= {rating};')
-    elif query:
-        games_list = api.get_games_test(f'search "{query}"; fields name, cover.url, genres.name;')
-    else:
-        games_list = []
+    platforms_filter = f'platforms = {str(platforms_id)};' if platform else ''
+    genres_filter = f'genres = {str(genres_id)};' if genre else ''
+    rating_filter = f'rating >= {rating};' if rating else ''
+    query_filter = f'search "{query}";' if query else ''
+    where_string = ''
+    fields_string = 'fields name, cover.url, genres.name;'
 
+    if platforms_filter and (genres_filter or rating_filter):
+        platforms_filter = platforms_filter[:-1]
+        platforms_filter += '&'
+
+    if genres_filter and rating_filter:
+        genres_filter = genres_filter[:-1]
+        genres_filter += '&'
+
+    if platforms_filter or genres_filter or rating_filter:
+        where_string = 'where '
+
+    body = f'{query_filter}{where_string}{platforms_filter}{genres_filter}{rating_filter}{fields_string}'
+
+    games_list = api.get_games_list(body)
     games_list = [game for game in games_list if game.get('cover')]
 
     return render(request, 'index.html', context={
                         'games': games_list,
                         'platforms': platforms_list,
-                        'genres': genres_list
+                        'genres': genres_list,
+                        'query': query,
+                        'platform': platform,
+                        'genre': genre,
+                        'rating': rating,
                 })
 
 
 def detailed_page(request, id):
     api = IgdbAPI()
-    game = api.get_games_test(f'fields name, platforms.name, genres.name,'
-                              f' screenshots.url, aggregated_rating, rating,'
-                              f' summary, first_release_date; where id = {id};')
+    game = api.get_game(id)
 
     return render(request, 'detailed_page.html', context={'game': game[0]})
